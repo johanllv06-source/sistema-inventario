191b4a8bb98f3c85ec48c518027f010364470a13
Johan Llashag <johanllv06@gmail.com>
2026-02-28 22:13:35 -0500

    Agregar página bonita

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def home():
-    return "Sistema de Inventario funcionando 🚀"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
