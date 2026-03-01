44fa075a781858174ccdec16e9f852c16dedf7bc
johanllv06-source <johanllv06@gmail.com>
2026-02-28 22:01:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def home():
     return "Sistema de Inventario funcionando 🚀"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
